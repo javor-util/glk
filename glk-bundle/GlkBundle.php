7564308daf31217d71c8dbadc517346ced461067
Bernhard Bauer <bernhard.bauer@bbmk.at>
2017-10-16 17:16:10 +0200

    .

---
+++ glk-bundle/GlkBundle.php
@@ -0,0 +1,1 @@
+<?php namespace Watkins\Glk\GlkBundle;use Symfony\Component\HttpKernel\Bundle\Bundle;use Symfony\Component\DependencyInjection\ContainerBuilder;class GlkBundle extends Bundle{public function build(ContainerBuilder $container){parent::build($container);}}
